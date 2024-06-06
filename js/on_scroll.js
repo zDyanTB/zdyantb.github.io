c5c1bf89cbfbcb2c6ec1aadf19d4f980153af728
zDyant <dyant@proton.me>
2024-06-05 23:32:19 -0300

    Update: animations, minor fixes

--- js/on_scroll.js
+++ js/on_scroll.js
@@ -8,12 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
         entry.target.classList.add('show');
       }
     });
-  }, { threshold: [0.2, 0.6] });
+  });
 
   // const hidden_elements = Array.prototype.slice.call(document.querySelectorAll(".reveal"), 0);
   setTimeout(function() {
     const hidden_elements = document.querySelectorAll(".reveal");
     console.log(hidden_elements);
     hidden_elements.forEach(el => observer.observe(el))
-  }, 600);
+  }, 800);
 });
+
